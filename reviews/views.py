e07e497f7e8ba0f5c9543858c9e66f3f28dabb46
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-04-30 16:04:33 -0300

    Forms built-in and submit

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here. 
+from .forms import ReviewForm
 
 def review(request):
-    return render(request, 'reviews/review.html')
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        
+        if form.is_valid():
+            print(form.cleaned_data)
+        return HttpResponseRedirect('/thank_you/')
+
+    form = ReviewForm()
+
+    return render(request, 'reviews/review.html', {
+        "form": form,
+    })
+
+def thank_you(request):
+    # This view can be used to display a thank you message after form submission
+    return render( request, 'reviews/thank_you.html', {
+        "has_error": False,
+    })

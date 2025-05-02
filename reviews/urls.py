53eea7133ca0b020411b51d39cd7fcb4bcab7025
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-05-02 11:16:26 -0300

    Path tweak

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.ReviewView.as_view(), name="review"),
     path("thank_you/", views.ThankYouView.as_view(), name="thank_you"),
     path("reviews", views.ReviewsListView.as_view(), name="reviews_list"),
-    path("revies/<int:pk>/", views.SingleReviewView.as_view(), name="single_review"),
+    path("reviews/<int:pk>/", views.SingleReviewView.as_view(), name="single_review"),
 ]

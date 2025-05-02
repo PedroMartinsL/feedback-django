e04d5d3e680979239801c12b4050348fc0aa16ea
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-05-02 10:27:05 -0300

    Showing a detail template

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view(), name="review"),
-    path("thank_you/", views.ThankYouView.as_view(), name="thank_you")
+    path("thank_you/", views.ThankYouView.as_view(), name="thank_you"),
+    path("reviews", views.ReviewsListView.as_view(), name="reviews_list"),
+    path("revies/<int:review_id>/", views.SingleReviewView.as_view(), name="single_review"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,8 @@ from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
 
+from .models import Review
+
 class ReviewView(View):
     def get(self, request):
         form = ReviewForm()
@@ -27,4 +29,21 @@ class ThankYouView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['message'] = 'This works!'
+        return context
+    
+class ReviewsListView(TemplateView):
+    template_name = 'reviews/review_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.all()
+        return context
+    
+class SingleReviewView(TemplateView):
+    template_name = 'reviews/single_review.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = self.kwargs.get('review_id')
+        context['review'] = Review.objects.get(id=review_id)
         return context
